a2ed40a47578bde3f04618525318486e43a24d10
EndoEnd <164733657+EndoEnd@users.noreply.github.com>
2024-08-16 18:37:32 +0900

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/partials/footer.blade.php -->
+
+<footer>
+
+    <a href="https://tenshoku-soudan.jp/"><img src="{{ asset('img/logo-1.webp') }}" alt="アーティックキャリア株式会社"></a>
+    <p>Created by Yoshitaka Endo</p>
+</footer>
+
---
+++ login.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <title>EAR ログイン画面</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <div class="anton-regular"><p>EAR</p>
+       </div>
+       <p1>アーティックキャリア株式会社<br>労務管理アプリ　β版</p1>
+    </header>
+
+    <div class="login">
+        <form action method="post">
+            <dl class="login">
+                <h2>ログイン</h2>
+                <div class="tab">
+                    <dd>
+                        <input type="text" name="mail" placeholder="メールアドレス">
+                    </dd>
+                    <dd>
+                        <input type="password" name="pass" placeholder="パスワード">
+                    </dd>
+                    <dd>
+                        <button type="submit">ログイン</button>
+                    </dd>
+                </div>
+            </dl>
+        </form>
+
+       
+
+    @include('partials.footer')
+</body>
+</html>
+
